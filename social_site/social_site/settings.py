eaee333416b7dbc7e990d7daf4ee7aa0fe97e1cc
ironsubhajit <mandalsubhajit655@gmail.com>
2020-12-09 19:49:00 +0530

    install bootstrap4 and accounts app

--- social_site/social_site/settings.py
+++ social_site/social_site/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [Path(BASE_DIR, 'static')]
+STATICFILES_DIRS = [Path(BASE_DIR, 'static'),]
