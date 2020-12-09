069cbe47bef61c264cef3935deff7d367f968469
ironsubhajit <mandalsubhajit655@gmail.com>
2020-12-09 20:26:43 +0530

    create signup view for accounts app

--- social_site/accounts/views.py
+++ social_site/accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+from . import forms
 
 
 class SignUp(CreateView):
-    pass
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "accounts/signup.html"
