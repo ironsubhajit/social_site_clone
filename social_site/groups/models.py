81b7ce889170c6ee05d16e0a3f933755da7a0e3e
ironsubhajit <mandalsubhajit655@gmail.com>
2020-12-16 19:11:33 +0530

    Add GroupMember model

--- social_site/groups/models.py
+++ social_site/groups/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+from django import template
+
+
+User = get_user_model()
+register = template.Library()
+
+
+class Group(models.Model):
+    pass
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name='membership', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_groups', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ("group", "user")
