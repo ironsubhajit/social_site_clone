3e4e62a213cec20e32e7d1209216ba4aea1c5073
ironsubhajit <mandalsubhajit655@gmail.com>
2020-12-26 21:28:45 +0530

    Create 'PostList' View

--- social_site/post/views.py
+++ social_site/post/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.http import Http404
+from django.views import generic
+
+from braces.views import SelectRelatedMixin
+
+from . import models
+from . import forms
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class PostList(SelectRelatedMixin, generic.ListView):
+    model = models.Post
+    select_related = ('user', 'group')
+
