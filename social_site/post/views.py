de26ea55ebec852f6bebc2e270e5575afa623044
ironsubhajit <mandalsubhajit655@gmail.com>
2020-12-26 21:37:33 +0530

    Create 'UserPosts' View

--- social_site/post/views.py
+++ social_site/post/views.py
@@ -17,3 +17,22 @@ class PostList(SelectRelatedMixin, generic.ListView):
     model = models.Post
     select_related = ('user', 'group')
 
+
+class UserPosts(generic.ListView):
+    model = models.Post
+    template_name = 'posts/user_post_list.html'
+
+    def get_queryset(self):
+        try:
+            self.post_user = User.objects.prefetch_related('posts').get(
+                username__iexact=self.kwargs.get("username")
+            )
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_user"] = self.post_user
+        return context
