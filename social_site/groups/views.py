01b43edc237bdbe067f0d1f40aa467b4c7c9cd29
ironsubhajit <mandalsubhajit655@gmail.com>
2021-01-15 21:58:21 +0530

    Add join group view

--- social_site/groups/views.py
+++ social_site/groups/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin
 )
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
 
 from django.urls import reverse
 
@@ -21,3 +23,24 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
+
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single",kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, messages=None, *args, **kwargs):
+        group = get_object_or_404(Group,slug=self.kwargs.get("slug"))
+
+        try:
+            GroupMember.objects.create(user=self.request.user,group=group)
+
+        except IntegrityError:
+            from django.contrib import messages
+            messages.warning(self.request,("Warning, already a member of {}".format(group.name)))
+
+        else:
+            messages.success(self.request,"You are now a member of the {} group.".format(group.name))
+
+        return super().get(request, *args, **kwargs)
